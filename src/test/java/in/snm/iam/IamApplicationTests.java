772b15708cbed79159c0a21a5920258253cf3a09
Shahid Nazir <shahidnazirmhd@gmail.com>
2024-07-23 05:50:37 +0000

    first commit

---
+++ src/test/java/in/snm/iam/IamApplicationTests.java
@@ -0,0 +1,13 @@
+package in.snm.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
