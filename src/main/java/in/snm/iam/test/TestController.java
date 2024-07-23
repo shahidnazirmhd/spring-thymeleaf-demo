6a2dfb4528c6cc8d4292bf7551c5eedc5d32122c
Shahid Nazir <shahidnazirmhd@gmail.com>
2024-07-23 06:06:34 +0000

    test

---
+++ src/main/java/in/snm/iam/test/TestController.java
@@ -0,0 +1,18 @@
+package in.snm.iam.test;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/test")
+@AllArgsConstructor
+public class TestController {
+
+    @GetMapping
+    public String testWithGreeting(){
+    return "Hello! This is project Iam";
+    }
+}
