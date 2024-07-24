c75730208c61d651936b25bc677cd2d758844125
Shahid Nazir <shahidnazirmhd@gmail.com>
2024-07-24 07:21:11 +0000

    Setting up project

--- src/main/java/in/snm/iam/test/TestController.java
+++ src/main/java/in/snm/iam/test/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/test")
+@RequestMapping(path = "test")
 @AllArgsConstructor
 public class TestController {
 
